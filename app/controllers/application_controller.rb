8ae988f3ccc36701281219e8aedd2fbd6a56fb5e
masafumikazama <masanomaildesu@yahoo.co.jp>
2022-03-26 18:51:10 +1000

    rubocop 修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base # rubocop:disable Style/Documentation
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV["SEND_MAIL"]
+  default from: ENV['SEND_MAIL']
   layout 'mailer'
 end
