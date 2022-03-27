47b1591f3c8dcf4bcce41a701ca46faae99652ae
masafumikazama <masanomaildesu@yahoo.co.jp>
2022-03-27 12:40:01 +1000

    Fix .rubocop.yml file
    Add Style/Documentation: Enabled: false

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::Base # rubocop:disable Style/Documentation
+class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
