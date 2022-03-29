acf13210faaf6e03f813e4dd653eb2ab3d190f55
masafumikazama <masanomaildesu@yahoo.co.jp>
2022-03-30 08:05:20 +1000

    add unprocessable_entity and fix private method

--- app/controllers/managers/account_controller.rb
+++ app/controllers/managers/account_controller.rb
@@ -3,29 +3,22 @@
 module Managers
   # 管理者のアカウント
   class AccountController < Managers::Base
-    before_action :set_manager, only: %i[show edit update]
 
     def show; end
 
     def edit; end
 
     def update
-      if @manager.update(manager_params)
-        sign_in(@manager, bypass: true)
+      if current_manager.update(manager_params)
+        sign_in(current_manager, bypass: true)
         redirect_to managers_show_path
       else
-        render :edit
+        render :edit, status: :unprocessable_entity
       end
     end
 
     def manager_params
       params.require(:manager).permit(:email, :password, :password_confirmation)
-    end
-
-    private
-
-    def set_manager
-      @manager = current_manager
     end
   end
 end
