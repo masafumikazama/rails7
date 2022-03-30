244e27be5ce8a26faa4f015fcc0f4c2c4665179f
masafumikazama <masanomaildesu@yahoo.co.jp>
2022-03-30 19:15:40 +1000

    set up basic CRUD for general_managers

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
 
   namespace :managers do
     resources :dashboards, only: [:index]
+    resources :general_managers
     resources :users, only: %i[index show]
     get :show, path: '/account', to: 'account#show'
     get :edit, path: '/account/edit', to: 'account#edit'
