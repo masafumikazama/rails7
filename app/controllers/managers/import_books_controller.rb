6b58a26b97b2364f0ccafcb6f71c5d518cf19701
shotaimai66 <changemymind6@gmail.com>
2022-04-05 07:40:56 +0900

    fix

--- app/controllers/managers/import_books_controller.rb
+++ app/controllers/managers/import_books_controller.rb
@@ -5,7 +5,11 @@ module Managers
     def new; end
 
     def create
-      Book.import(params[:file])
+      Shoryuken.configure_client do |config|
+        sqs_client = config.sqs_client
+        queue_url = sqs_client.get_queue_url(queue_name: 'csv_import_worker')['queue_url']
+        sqs_client.send_message(queue_url: queue_url, message_body: { "hoge": 'fuge' }.to_json)
+      end
       redirect_to root_path
     end
   end
