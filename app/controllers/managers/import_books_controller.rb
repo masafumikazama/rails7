959eee74b039016e9bd91e7203e3c643b14545ff
shotaimai66 <changemymind6@gmail.com>
2022-04-05 20:35:04 +0900

    fix

--- app/controllers/managers/import_books_controller.rb
+++ app/controllers/managers/import_books_controller.rb
@@ -5,12 +5,17 @@ module Managers
     def new; end
 
     def create
+      # csvを一旦ストレージに保存（画像保存と一緒）、参照情報だけDBに保存（新たにテーブルが必要）。
+      book_csv = BookCsv.new(params)
+      book_csv.save!
+      # 以下でキューにメッセージを送る
       Shoryuken.configure_client do |config|
         sqs_client = config.sqs_client
         queue_url = sqs_client.get_queue_url(queue_name: 'csv_import_worker')['queue_url']
-        sqs_client.send_message(queue_url: queue_url, message_body: { "hoge": 'fuge' }.to_json)
+        sqs_client.send_message(queue_url: queue_url, message_body: { "book_csv_id": book_csv.id }.to_json)
       end
-      redirect_to root_path
+      flash[:notice] = "インポート処理を開始しました！"
+      redirect_to managers_import_books_path
     end
   end
 end
--- app/workers/csv_import_worker.rb
+++ app/workers/csv_import_worker.rb
@@ -4,9 +4,10 @@ class CsvImportWorker
   shoryuken_options queue: 'csv_import_worker', auto_delete: true, body_parser: :json
 
   def perform(sqs_msg, body)
-    puts sqs_msg, body
-    puts "処理開始"
-    CSV.foreach('./import_csv_rails7.csv', headers: true) do |row|
+    puts "CSVインポート処理開始"
+    # キューのメッセージにレコードのIDを入れてあるのでそれを元にレコードを特定
+    csv = BookCsv.find(body['book_csv_id']).csv # book_csvのcsvが格納されているカラムを参照
+    CSV.foreach(csv, headers: true) do |row|
       book = Book.new
       book.attributes = row.to_hash.slice(*updatable_attributes)
       book.save!
