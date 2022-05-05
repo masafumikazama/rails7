b647a8e6f0fd6ce9d3d8c43e0948fd6823525514
masafumikazama <masanomaildesu@yahoo.co.jp>
2022-05-05 13:50:15 +1000

    ポーリンング処理のテスト作成途中

--- spec/system/managers/import_books_spec.rb
+++ spec/system/managers/import_books_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'CSVファイルインポートの画面遷移', type: :system do
 
-  before(:each) do
-    login_manager
-  end
+  # before(:each) do
+  #   login_manager
+  # end
 
-  file = CSV.generate do |csv|
-    csv << ["name", "facebook_url"]
-    csv << ["artict1", "nil"]
-    csv << ["artict2", "facebook_url"]
-  end
+  # file = CSV.generate do |csv|
+  #   csv << ["name", "facebook_url"]
+  #   csv << ["artict1", "nil"]
+  #   csv << ["artict2", "facebook_url"]
+  # end
 
-  context 'CSVファイルインポートの画面遷移' do
-    it 'CSVファイルをインポートできることを確認' do
-      visit new_managers_import_book_path
+  # context 'CSVファイルインポートの画面遷移' do
+  #   it 'CSVファイルをインポートできることを確認' do
+  #     visit new_managers_import_book_path
 
 
-    end
-  end
+  #   end
+  # end
 end
